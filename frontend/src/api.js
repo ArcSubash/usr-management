92e385c91b6faf11a84c72888f349c6c2219b8d0
Subash B <subash110607@gmail.com>
2026-03-08 12:18:46 +0530

    Deploy 1

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "/api", // uses Vite proxy
+    baseURL: "https://usr-mng-deploy-bknd.onrender.com/api",
 });
 
-// Attach token automatically (if present)
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
     if (token) config.headers.Authorization = `Bearer ${token}`;
